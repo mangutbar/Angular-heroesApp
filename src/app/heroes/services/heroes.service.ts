e62ef32964c15bac398c2a4980f44e1b325e04f7
Manuel Gutierrez <magutbar@gmail.com>
2023-03-21 14:02:52 +0100

    Primer commit heroesApp

---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Heroe } from '../interfaces/heroes.interface';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+  constructor( private http: HttpClient ) { }
+
+  getHeroes(): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+  }
+
+  getHeroePorId( id: string): Observable<Heroe> {
+    return this.http.get<Heroe>(`http://localhost:3000/heroes/${ id }`);
+  }
+}
