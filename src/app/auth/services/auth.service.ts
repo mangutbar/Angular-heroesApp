47e27a98264721f86225032f69eb4ba1892fa7f5
Manuel Gutierrez <magutbar@gmail.com>
2023-03-23 10:36:37 +0100

    Final heroesApp y proteccion de rutas

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { pipe, tap } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth.interface';
 
@@ -18,10 +19,25 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
+  verificaAutenticacion(): Observable<boolean> {
+    if(!localStorage.getItem('id')) {
+      return of(false);
+    }
+
+    return this.http.get<Auth>(`${ this.baseUrl }/usuarios/1`)
+              .pipe(
+                map( auth => {
+                  this._auth = auth;
+                  return true;
+                })
+              );
+  }
+
   login() {
     return this.http.get<Auth>(`${ this.baseUrl }/usuarios/1`)
               .pipe(
-                tap( auth => this._auth = auth)
+                tap( auth => this._auth = auth),
+                tap( auth => localStorage.setItem('id', auth.id))
               );
   }
 }
